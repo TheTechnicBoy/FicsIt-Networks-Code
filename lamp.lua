3cd9da88fdb3e32800fed27ac1fd21704b7d0d8a
TheTechnicBoy <51886489+TheTechnicBoy@users.noreply.github.com>
2021-09-01 20:44:49 +0200

    Update lamp.lua

--- lamp.lua
+++ lamp.lua
@@ -1,6 +1,7 @@
 local switch = component.proxy(component.findComponent(findClass("Build_PowerSwitch_C")))[1]
 local panel = component.proxy(component.findComponent(findClass("MCP_1Point_Center_C")))[1]
 local s = panel:getYModule(0)
+print("Script: Lamp.lua , was loaded successfully")
 
 while true do
    if s.state == true then
