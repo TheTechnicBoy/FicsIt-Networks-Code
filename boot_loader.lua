69d1852128ef4eaa7401e1bc7067691c80e6228b
TheTechnicBoy <51886489+TheTechnicBoy@users.noreply.github.com>
2021-09-07 15:58:37 +0200

    Update boot_loader.lua

--- boot_loader.lua
+++ boot_loader.lua
@@ -1,6 +1,14 @@
-mount = filesystem.loadFile("/mount_drive.lua")
-
-if mount then
-	computer.beep(5)
-	mount()
+filesystem.initFileSystem("/dev")
+local drive = ""
+for _,f in pairs(filesystem.childs("/dev")) do
+	if not (f == "serial") then
+		drive = f
+		break
+	end
 end
+if drive:len() < 1 then
+	print("ERROR! Unable to find filesystem to install on! Please insert a drive or floppy!")
+	computer.beep(0.2)
+	return
+end
+filesystem.mount("/dev/" .. drive, "/")
