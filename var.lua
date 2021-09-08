e5772e80eec981163a1155009a2fb8ab7427faf8
TheTechnicBoy <51886489+TheTechnicBoy@users.noreply.github.com>
2021-09-08 18:28:11 +0200

    Update var.lua

--- var.lua
+++ var.lua
@@ -1,4 +1,6 @@
 function var(var, value)
+  local path = filesystem.path("/","VAR")
+  filesystem.createDir(path)
   local file = filesystem.open(var .. ".ttb", "w")
   file:write(var .. " = " .. value)
   file:close()
