57a4d5797f000acdbe8213af6f8674a7fec54574
TheTechnicBoy <51886489+TheTechnicBoy@users.noreply.github.com>
2021-09-12 15:22:10 +0200

    Create load_function.lua

---
+++ load_function.lua
@@ -0,0 +1,14 @@
+  func_var = filesystem.loadFile("var.lua")
+  func_var()
+
+  func_file = filesystem.loadFile("download.lua")
+  func_file()
+
+  func_rss = filesystem.loadFile("RSS.lua")
+  func_rss()
+
+  func_folder = filesystem.loadFile("folder.lua")
+  func_folder()
+
+  func_color = filesystem.loadFile("color.lua")
+  func_color()
