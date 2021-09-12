038cc81d6f56c0904cbf26adb29c2c693807e408
TheTechnicBoy <51886489+TheTechnicBoy@users.noreply.github.com>
2021-09-12 16:22:17 +0200

    Create deletefilesystem.lua

---
+++ deletefilesystem.lua
@@ -0,0 +1,10 @@
+function deletefilesystem()
+print("The Filesystem will be removed in 5-sec till the high beeps!")
+for i=1, 5, 1 do
+	event.pull(1)
+	print(i .. "...")
+	computer.beep(0.7)
+end
+filesystem.removeFileSystem("/dev")
+computer.setEEPROM("")
+end
