d5e814fe371f39f324c95e2222d72013cf4ca16b
TheTechnicBoy <Finn.Grotkopf@t-online.de>
2021-12-03 18:19:46 +0100

    ttb_os folder

---
+++ ttb_os/color.lua
@@ -0,0 +1,65 @@
+   function red_1(device)
+    device:setColor(1,0,0,1)
+   end
+   
+   function green_1(device)
+    device:setColor(0,1,0,1)
+   end
+   
+   function blue_1(device)
+    device:setColor(0,0,1,0)
+   end
+   
+   function yellow_1(device)
+    device:setColor(1,1,0,1)
+   end
+   
+   function purple_1(device)
+    device:setColor(1,0,1,1)
+   end
+   
+   function cyan_1(device)
+    device:setColor(0,1,1,1)
+   end
+   
+   function black_1(device)
+    device:setColor(0,0,0,1)
+   end
+   
+   function white_1(device)
+    device:setColor(1,1,1,1)
+   end
+
+
+
+   function red_0(device)
+    device:setColor(1,0,0,0)
+   end
+   
+   function green_0(device)
+    device:setColor(0,1,0,0)
+   end
+   
+   function blue_0(device)
+    device:setColor(0,0,1,0)
+   end
+   
+   function yellow_0(device)
+    device:setColor(1,1,0,0)
+   end
+   
+   function purple_0(device)
+    device:setColor(1,0,1,0)
+   end
+   
+   function cyan_0(device)
+    device:setColor(0,1,1,0)
+   end
+   
+   function black_0(device)
+    device:setColor(0,0,0,0)
+   end
+   
+   function white_0(device)
+    device:setColor(1,1,1,0)
+   end
