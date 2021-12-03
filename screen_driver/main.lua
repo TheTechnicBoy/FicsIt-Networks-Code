7fcf8c73062ee89e61a3540fab72c7fe56279da2
TheTechnicBoy <Finn.Grotkopf@t-online.de>
2021-12-03 20:23:57 +0100

    -

--- screen_driver/main.lua
+++ screen_driver/main.lua
@@ -1,1 +1,50 @@
-print("Hi")
+--setup components
+gpu = computer.getPCIDevices(findClass("GPUT1"))[1]
+Screen = computer.getPCIDevices(findClass("FINComputerScreen"))[1]
+if not Screen then
+    computer.beep(1)
+    print("Could not find Screen")
+end
+
+--event listen
+event.listen(gpu)
+
+
+--screen-setup
+gpu:bindScreen(ScreenComponent)
+screenW, screenH = gpu:getSize()
+gpu:fill(0, 0, screenW, screenH, " ") 
+gpu:flush()
+
+
+
+while true do
+
+    Controller()
+
+    for i,v in ipairs(Controller) do
+         if i%2 == 0 then
+         else
+         print(i)
+         end
+    end
+
+
+    e, s, x, y = event.pull()
+
+    --e = event
+    --s = sender
+    --x = 1 parm
+    --y = 2 parm
+
+    if e == "OnMouseDown" then
+        --print("Down")
+    elseif e == "OnMouseUp" then
+        --print("UP")
+    elseif e == "OnMouseMove" then
+        --print(x,y)
+    end
+
+    gpu:flush()
+
+end
--- screen_driver/setup.lua
+++ screen_driver/setup.lua
@@ -1,6 +1,5 @@
 url = "https://raw.githubusercontent.com/TheTechnicBoy/FicsIt-Networks-Code/main/screen_driver/main.lua"
 
-
 local internet = computer.getPCIDevices(findClass("FINInternetCard"))[1]
 local code, data = internet:request(url, "GET", ""):await()
 
@@ -10,4 +9,22 @@ if code ~= 200 or not data then
     return
 end
 
-data()
+main = load(data)
+
+
+function Controller()
+    
+    Controller = {}
+
+    Controller[1] = "Name"
+    Controller[2] = "ID"
+
+
+
+
+
+end
+
+
+
+main()
