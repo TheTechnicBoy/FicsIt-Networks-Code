d1c1dbf7cf189dcc36917e862b28bc78b341a075
TheTechnicBoy <Finn.Grotkopf@t-online.de>
2021-12-03 22:33:00 +0100

    Create setup.lua

---
+++ standby/setup.lua
@@ -0,0 +1,31 @@
+url = "https://raw.githubusercontent.com/TheTechnicBoy/FicsIt-Networks-Code/main/standby/main.lua"
+
+local internet = computer.getPCIDevices(findClass("FINInternetCard"))[1]
+local code, data = internet:request(url, "GET", ""):await()
+
+if code ~= 200 or not data then
+    print("ERROR! Unable to request file")
+    computer.beep(1)
+    return
+end
+
+main = load(data)
+
+
+
+function objects()
+    
+    Controller = {}
+
+    Controller[1] = "Name"
+    Controller[2] = "ID"
+    Controller[3] = "Name"
+    Controller[4] = "ID"
+    --etc.
+
+
+end
+
+
+
+main()
