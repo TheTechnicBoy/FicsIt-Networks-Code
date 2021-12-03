df6d5d794da7eaca88639b94173e530382dd82ed
TheTechnicBoy <Finn.Grotkopf@t-online.de>
2021-12-03 18:36:30 +0100

    -

---
+++ screen_driver/setup.lua
@@ -0,0 +1,13 @@
+url = "https://raw.githubusercontent.com/TheTechnicBoy/FicsIt-Networks-Code/main/screen_driver/main.lua"
+
+
+local internet = computer.getPCIDevices(findClass("FINInternetCard"))[1]
+local code, data = internet:request(url, "GET", ""):await()
+
+if code ~= 200 or not data then
+    print("ERROR! Unable to request file")
+    computer.beep(1)
+    return
+end
+
+data()
